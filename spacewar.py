ee88230a6abe5980827c147df9dc0b6022c017f4
praiseunite <praiseubong28@gmail.com>
2024-10-15 17:13:27 +0100

    box drawn

---
+++ spacewar.py
@@ -0,0 +1,15 @@
+import os
+import random
+
+#Import turtle module
+import turtle
+turtle.fd(0) #fd is short for forward for the window to open on mac 
+turtle.speed(0) #Sets the speed of the turtle module to 0 which is the speed of the animation.
+turtle.bgcolor("black") #Sets the background color to black 
+
+
+
+
+
+
+delay = input("Press enter to finish. > ")
