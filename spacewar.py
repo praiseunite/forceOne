78f692ec33aa19b0cf939beebd4a3879fb119e1b
praiseunite <praiseubong28@gmail.com>
2024-10-16 22:31:36 +0100

    added boarders

--- spacewar.py
+++ spacewar.py
@@ -24,22 +24,50 @@ class Sprite(turtle.Turtle):
         self.fd(self.speed)
 
 class Player(Sprite):  #Inherits from Sprite class  #Player class is a subclass of the Sprite class
-     def __init__(self, sprite_shape, color, startx, starty): #Constructor  __init__ is a special method in Python classes, it is the constructor method for a class. It is called when an object of the class is created.
+    def __init__(self, sprite_shape, color, startx, starty): #Constructor  __init__ is a special method in Python classes, it is the constructor method for a class. It is called when an object of the class is created.
         Sprite.__init__(self, sprite_shape, color, startx, starty)  #Calls the constructor of the Sprite class and passes the parameters to it.  #The super() function is used to give access to methods and properties of a parent or sibling class.
         self.speed = 2
         self.lives = 3
         
-     def turn_left(self):  #Method to turn the player to the left and it needs to be connected to the arrow key on the keyboard.
+    def turn_left(self):  #Method to turn the player to the left and it needs to be connected to the arrow key on the keyboard.
         self.lt(45)
         
-     def turn_right(self):
+    def turn_right(self):
         self.rt(45)
         
-     def accelerate(self):
+    def accelerate(self):
         self.speed += 1
         
-     def decelerate(self):
+    def decelerate(self):
         self.speed -= 1
+        
+class Game():
+    def __init__(self):
+        self.level = 1
+        self.score = 0
+        self.state = "playing"
+        self.pen = turtle.Turtle()
+        self.lives = 3
+        
+    def draw_border(self):
+        #Draw border
+        self.pen.speed(0)
+        self.pen.color("white")
+        self.pen.pensize(3)
+        self.pen.penup()
+        self.pen.goto(-300, 300)
+        self.pen.pendown()
+        for side in range(4):
+            self.pen.fd(600)
+            self.pen.rt(90)
+        self.pen.penup()
+        self.pen.ht()
+
+#Create game object
+game = Game()
+
+#Draw the game border
+game.draw_border()
 
 #Create my Sprite 
 player = Player("triangle", "white", 0, 0)
@@ -51,7 +79,7 @@ turtle.onkey(player.accelerate, "Up")
 turtle.onkey(player.decelerate, "Down")
 turtle.listen()
 
-#MAin game loop
+#Main game loop
 while True:
     player.move()
     
